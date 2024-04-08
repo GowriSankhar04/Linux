206c63d021af12517b24ec6f8b6303b9fe029041
GowriSankhar04 <166432636+GowriSankhar04@users.noreply.github.com>
2024-04-08 23:46:07 +0530

    centroid_of_protein.sh
    Gets the coordinates from the pdb and finds the centroid

--- centroid_of_protein.sh
+++ centroid_of_protein.sh
@@ -1,1 +1,9 @@
-grep -v "GeneID" a1.txt | awk '{for(i=0;i < NR;i++) {sum=0;sum=$2+$3+$4;sum=sum/3;if (sum > 10) print $1}}'|sort|uniq
+for i in 2dsq.pdb
+
+do
+
+echo " running on $i"
+
+awk '$1=="ATOM" && $3=="CA" {sum_x+=$7;sum_y+=$8;sum_z+=$9;count++}END {print sum_x/count,sum_y/count,sum_z/count}' $i 
+
+done
